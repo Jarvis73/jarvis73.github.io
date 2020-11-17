0482032367e673398916bb2ff82a0287d9916b50
Jarvis73 <zjw.math@qq.com>
2020-11-17 22:05:12 +0800

    add post

--- js/index_page.js
+++ js/index_page.js
@@ -1,13 +1,6 @@
 $('#myPager').pager({
     recPerPage: 1,
     lang: 'zh_cn',
-    onPageChange: function(state, oldState) {
-        if (state.page !== oldState.page) {
-            if (state.page == 1) {
-                console.log(window.location.pathname);
-            }
-        }
-    },
     linkCreator: function(page, pager) {
         if (page == 1)
             return "/";
